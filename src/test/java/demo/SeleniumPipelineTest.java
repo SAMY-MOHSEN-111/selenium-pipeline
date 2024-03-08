015925c04e6398d29b4f1fdca70c565ad71c8748
Samy Mohsen <11410120200220@stud.cu.edu.eg>
2024-03-08 13:32:57 +0200

    headless mode

--- src/test/java/demo/SeleniumPipelineTest.java
+++ src/test/java/demo/SeleniumPipelineTest.java
@@ -3,6 +3,7 @@ package demo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,13 +11,16 @@ public class SeleniumPipelineTest {
 
     @Test
     public void openSeleniumAndNavigateToGoogle(){
-        System.out.println("test start");
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+
         WebDriverManager.chromedriver().setup();
-        var driver =  new ChromeDriver();
-        System.out.println("before browser open");
+        var driver =  new ChromeDriver(chromeOptions);
         driver.get("https://google.com");
         var element = driver.findElement(By.id("APjFqb"));
         Assert.assertTrue(element.isDisplayed());
-        driver.close();
+        System.out.println("worked");
+        System.out.println(driver.getTitle());
+        driver.quit();
     }
 }
