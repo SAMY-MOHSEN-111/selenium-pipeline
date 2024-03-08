adacc5ead4c456144718114566e460cfff822b3e
Samy Mohsen <11410120200220@stud.cu.edu.eg>
2024-03-08 14:13:09 +0200

    docker connection

--- src/test/java/demo/SeleniumPipelineTest.java
+++ src/test/java/demo/SeleniumPipelineTest.java
@@ -4,23 +4,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SeleniumPipelineTest {
 
     @Test
-    public void openSeleniumAndNavigateToGoogle(){
+    public void openSeleniumAndNavigateToGoogle() throws MalformedURLException {
+        var url = new URL("http://localhost:4444/wd/hub");
         var chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+//        chromeOptions.addArguments("--headless");
 
-        WebDriverManager.chromedriver().setup();
-        var driver =  new ChromeDriver(chromeOptions);
+        var driver = new RemoteWebDriver(url, chromeOptions);
+
         driver.get("https://google.com");
         var element = driver.findElement(By.id("APjFqb"));
         Assert.assertTrue(element.isDisplayed());
         System.out.println("worked");
         System.out.println(driver.getTitle());
-        driver.quit();
+//        driver.quit();
     }
 }
