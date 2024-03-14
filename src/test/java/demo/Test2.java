2cd69b21ba77d2f8a515db77d6b08d9f32546580
Samy Mohsen <11410120200220@stud.cu.edu.eg>
2024-03-14 19:27:11 +0200

    adding docker compose file

--- src/test/java/demo/Test2.java
+++ src/test/java/demo/Test2.java
@@ -4,6 +4,7 @@ import org.example.actions.BrowserConfiguration;
 import org.example.actions.Screenshot;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -26,28 +27,29 @@ public class Test2 {
     public void takeScreenshotOfFacebookPageTest() throws IOException {
         driver.get("https://www.facebook.com");
         Screenshot.takeScreenshot(driver);
-        driver.close();
     }
 
     @Test
     public void takeScreenshotOfGoogle() throws IOException {
         driver.get("https://www.google.com");
         Screenshot.takeScreenshot(driver);
-        driver.close();
     }
 
     @Test
     public void takeScreenshotOfWiki() throws IOException {
         driver.get("https://www.wikipedia.org");
         Screenshot.takeScreenshot(driver);
-        driver.close();
     }
 
     @Test
     public void takeScreenshotOfX() throws IOException {
         driver.get("https://www.x.org");
         Screenshot.takeScreenshot(driver);
-        driver.close();
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
     }
 }
 
